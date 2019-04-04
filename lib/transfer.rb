255da4c33bda4b4cf0a2a6ae67a7b4c5b45062e3
Dwake5 <wakeling101@hotmail.com>
2019-04-04 13:07:00 +0100

    updating master branch

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
+#require_relative './bank_account.rb'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if @status == "pending" && valid?
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    else
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+
 end
