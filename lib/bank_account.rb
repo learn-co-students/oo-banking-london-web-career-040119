07ac5cd83ac7724069adeacaaa0bbd4f889c4aff
A Hashim <ahashim983@yahoo.com>
2019-04-04 15:07:08 +0100

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
+
+require_relative '../lib/transfer.rb'
 class BankAccount
+
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  @@all_accounts = []
+
+  def initialize(name,balance=1000,status='open')
+    @name = name
+    @balance = balance
+    @status = status
+    @@all_accounts << self
+  end
+
+  def deposit(amount)
+    @balance = @balance+amount
+  end
+
+  def display_balance
+    b = @balance
+    return "Your balance is $#{b}."
+  end
+
+  def valid?
+    return true if self.status == 'open' && self.balance>0
+    return false
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+
+  def self.get_all_accounts
+    @@all_accounts
+  end
 
 end
---
+++ run.rb
@@ -0,0 +1,2 @@
+require_relative '../lib/bank_account.rb'
+require_relative '../lib/transfer.rb'
