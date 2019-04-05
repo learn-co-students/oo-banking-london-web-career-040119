a73d1faa8f0b609b9bb6b13a30b3f9603b308ff0
francosta <francisco@fcosta.pt>
2019-04-05 08:42:18 +0100

    did all of the methods in bank_accound.rb and most in transfer.rb

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :transfer_amount
+  attr_accessor :status, :transfer_amount
+
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+  end
+
+  def valid?
+    if @receiver.valid? == true && @sender.valid? == true then true end
+  end
+
+  def execute_transaction
+
+  end
+
+
 end
